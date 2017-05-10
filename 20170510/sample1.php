74dffac9bd27738c43c06c8afc060387af09f22d
shikota <shikota1013@yahoo.co.jp>
2017-05-10 11:37:04 +0900

    Switch case文

---
+++ 20170510/sample1.php
@@ -0,0 +1,19 @@
+<?php
+$level1 = "B";
+
+switch($level1){
+
+    case"A":
+        print("素晴らしい");
+        break;
+    case"B":
+        print("いいね");
+        break;
+    case"C":
+        print("悪くはないけど");
+        break;
+    default:
+        print("頑張って");
+        break;
+}
+?>
