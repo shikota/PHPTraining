e5b8ed46abadb4c2ceb88b6647ff4fec08d594a7
shikota <shikota1013@yahoo.co.jp>
2017-05-10 13:11:24 +0900

    switchの課題練習

--- 20170510/kadai1.php
+++ 20170510/kadai1.php
@@ -3,16 +3,19 @@ $color = "赤";
 
 switch ($color) {
     case "赤":
-        echo "あなたの好きな色が赤！";
+        echo "あなたの好きな色は赤";
         break;
     case "青":
-        echo "あなたの好きな色が青！";
+        echo "あなたの好きな色は青！";
         break;
     case"緑":
         echo "あなたの好きな色は緑！";
         break;
+    case"黄"
+        echo "あなたの好きな色は黄！";
+        break;
     default:
-        echo "あなたの好きな色が上の中にない！";
+        echo "選択肢のどれでもない・・・だと・・・！";
         break;
 }
 ?>
