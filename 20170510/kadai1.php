fca2245a7c145a7376b3768794da529b9d4f092d
shikota <shikota1013@yahoo.co.jp>
2017-05-10 11:52:27 +0900

    課題練習

---
+++ 20170510/kadai1.php
@@ -0,0 +1,18 @@
+<?php
+$color = "赤";
+
+switch ($color) {
+    case "赤":
+        echo "あなたの好きな色が赤！";
+        break;
+    case "青":
+        echo "あなたの好きな色が青！";
+        break;
+    case"緑":
+        echo "あなたの好きな色は緑！";
+        break;
+    default:
+        echo "あなたの好きな色が上の中にない！";
+        break;
+}
+?>
