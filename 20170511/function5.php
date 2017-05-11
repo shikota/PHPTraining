59fa7493962ad4306ef6f6ac1daf387392bf1ea2
shikota <shikota1013@yahoo.co.jp>
2017-05-11 11:51:36 +0900

    訂正

--- 20170511/function5.php
+++ 20170511/function5.php
@@ -4,6 +4,6 @@ function sum($x, $y){
     return $z;
 }
 echo "5 + 10 =".sum(5,10)."<br>";
-echo "7 + 13 =".sum(5,10)."<br>";
-echo "2 + 4 =".sum(5,10)."<br>";
+echo "7 + 13 =".sum(7,13)."<br>";
+echo "2 + 4 =".sum(2,4)."<br>";
 ?>
