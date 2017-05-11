f556701d3b5c1c4698ed4175df608dff41db2655
shikota <shikota1013@yahoo.co.jp>
2017-05-11 10:31:45 +0900

    多次元配列

--- 20170510/operator_kadai3.php
+++ 20170510/operator_kadai3.php
@@ -1,12 +1,16 @@
 <?php
 echo "<table border = 1>";
+$j = 1;
 for($i = 1;$i <= 10;$i++){
     echo "<tr>";
-    for($j = 1;$j <= 10;$j++){
+    if($j <= 10){
         $x = $i * $j;
         echo "<td>". $x ."</td>";
+    }else {
+        echo "<br/>";
     }
     echo "</tr>";
+    $j++;
 }
 echo "</table>";
 ?>
