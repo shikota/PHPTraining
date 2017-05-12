653acf8c7bbd4770bb6fdf1492c1de451b2ee6f5
shikota <shikota1013@yahoo.co.jp>
2017-05-12 17:31:33 +0900

    月曜日続きを

--- 20170512/sample.php
+++ 20170512/sample.php
@@ -1,13 +1,14 @@
 <?php
 date_default_timezone_set('Asia/Tokyo');
 $fp = fopen("count.txt", "r+");
-$count = fgets($fp, 10);
+$count = fgets($fp, 1000);
 $count++;
+$pieces = explode(",", $count);
 $count = trim($count);
 rewind($fp);
 fputs($fp, $count);
 fclose($fp);
-echo 'あなたは'.$count.'人目のお客様です'."<br>";
+echo 'あなたは'.$pieces[0].'人目のお客様です'."<br>";
 $datetime = new Datetime();
 $week = array("日", "月", "火", "水", "木", "金", "土");
 if (date("w") == 0) {
@@ -23,5 +24,6 @@ if (date("w") == 0) {
 }else if(date("w") == 5) {
     $w = "(金)";
 }else $w = "(土)";
-echo date("Y m/d $w H:i");
+echo date("Y m/d $w H:i")."<br>";
+echo $pieces[0];
 ?>
